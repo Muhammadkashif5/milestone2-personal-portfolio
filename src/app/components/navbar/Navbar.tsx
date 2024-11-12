b9767d0737cee91db3dc50ff927b4015c83561d6
Muhammadkashif5 <muhammad.kashif7016992@gmail.com>
2024-11-12 23:32:41 +0500

    canes

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useCallback, useEffect, useState } from "react";
 import "./Navbar.css";
@@ -6,43 +6,45 @@ import "./Navbar.css";
 const Navbar = () => {
   // show menu on medium and small devices
   const [showmenu, setShowmenu] = useState(false);
+  
+  useEffect(() => {
+    // change active section background header
 
-  // change active section background header
-  const scrollActive = useCallback(() => {
-    let section: NodeListOf<Element> | any = [];
-    if (!section.length) {
-      section = document.querySelectorAll("section[id]");
-    }
-
-    const scrollY = window.pageYOffset + window.innerHeight / 3;
-
-    section.forEach((current: any) => {
-      const sectionHeight = current.offsetHeight;
-      const sectionTop = current.offsetTop - 50;
-      const sectionId = current.getAttribute("id");
-
-      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-        document
-          ?.querySelector(".nav_menu a[href*=" + sectionId + "]")
-          ?.classList.add("active_link");
-      } else {
-        document
-          ?.querySelector(".nav_menu a[href*=" + sectionId + "]")
-          ?.classList.remove("active_link");
+    const scrollActive = () => {
+      let section: NodeListOf<Element> | any = [];
+      if (!section.length) {
+        section = document.querySelectorAll("section[id]");
       }
-    });
+
+      const scrollY = window.pageYOffset + window.innerHeight / 3;
+
+      section.forEach((current: any) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          document
+            ?.querySelector(".nav_menu a[href*=" + sectionId + "]")
+            ?.classList.add("active_link");
+        } else {
+          document
+            ?.querySelector(".nav_menu a[href*=" + sectionId + "]")
+            ?.classList.remove("active_link");
+        }
+      });
+    };
+
+    window.addEventListener("scroll", scrollActive);
   }, []);
 
   useEffect(() => {
-    window.addEventListener("scroll", scrollActive);
-  }, [scrollActive]);
-
-  //Adding box Shadow
-  window.addEventListener("scroll", function () {
-    const header = document.querySelector(".header");
-    if (this.scrollY >= 30) header?.classList.add("scroll_header");
-    else header?.classList.remove("scroll_header");
-  });
+    window.addEventListener("scroll", function () {
+      const header = document.querySelector(".header");
+      if (this.scrollY >= 30) header?.classList.add("scroll_header");
+      else header?.classList.remove("scroll_header");
+    });
+  }, []);
 
   const menuHandler = () => {
     setShowmenu(false);
@@ -52,11 +54,10 @@ const Navbar = () => {
     <header className="header">
       <nav className="nav container">
         <div className="nav_logo" data-aos="fade-right">
-              Muhammad Kashif
+          Muhammad Kashif
         </div>
         <div
           className={showmenu ? "nav_menu show_menu" : "nav_menu"}
-          data-aos={window.innerWidth > 768 ? "fade-left" : null}
         >
           <ul className="nav_list">
             <li className="nav_item">
@@ -73,7 +74,7 @@ const Navbar = () => {
                 <i className="uil uil-user nav_icon"></i> About
               </a>
             </li>
- 
+
             <li className="nav_item">
               <a href="#resume" className="nav_link" onClick={menuHandler}>
                 <i className="uil uil-graduation-cap nav_icon"></i> Resume
@@ -91,7 +92,10 @@ const Navbar = () => {
             </li>
           </ul>
 
-          <i className="uil uil-times nav_close" onClick={() => setShowmenu(false)}></i>
+          <i
+            className="uil uil-times nav_close"
+            onClick={() => setShowmenu(false)}
+          ></i>
         </div>
 
         <div className="nav_toggle" onClick={() => setShowmenu(true)}>
